41a3d9f59015312be323a433ef13084c248108fe
Leelambika1605 <leelambika.r1605@gmail.com>
2026-03-16 20:42:01 +0530

    Added Bitwise Operators example in Java
    This Java program demonstrates the use of Bitwise Operators in Java.
    
    Operators covered:
    - AND (&)
    - OR (|)
    - XOR (^)
    
    The program performs these operations on the integers 5 and 6 and prints the results.

---
+++ bitMain.java
@@ -0,0 +1,13 @@
+public class bitMain{
+    public static void main(String[] args) {
+        //bitwise Operator
+        //binary and (&)
+        System.out.println(5 & 6);
+
+        //binary or (|)
+        System.out.println(5 | 6);
+
+        //binary xor (^)
+        System.out.println(5 ^ 6);
+    }
+}
